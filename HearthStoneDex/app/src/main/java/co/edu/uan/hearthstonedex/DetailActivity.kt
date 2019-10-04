40f3196df5f3640e77e541f4dbb0ea737a88c22b
wjfatuan <32616077+wjfatuan@users.noreply.github.com>
2019-10-04 09:34:03 -0500

    HearthStoneDex Visual Effect

--- HearthStoneDex/app/src/main/java/co/edu/uan/hearthstonedex/DetailActivity.kt
+++ HearthStoneDex/app/src/main/java/co/edu/uan/hearthstonedex/DetailActivity.kt
@@ -1,5 +1,6 @@
 package co.edu.uan.hearthstonedex
 
+import android.animation.Animator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_detail.*
@@ -8,6 +9,8 @@ import androidx.core.app.ComponentActivity.ExtraData
 import androidx.core.content.ContextCompat.getSystemService
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.util.Log
+import com.daimajia.androidanimations.library.Techniques
+import com.daimajia.androidanimations.library.YoYo
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import java.lang.Exception
@@ -32,7 +35,6 @@ class DetailActivity : AppCompatActivity() {
         val text = intent.getStringExtra("cardText")
         //var imageId = resources.getIdentifier(image,"drawable", packageName)
         val url = "https://d15f34w2p8l1cc.cloudfront.net/hearthstone/ba76f5895f734927f34cbeb6938946caaaa261d1b7cb7d54282cb34b8b810025.png"
-        cardText.text = text
         //cardImage.setImageResource(imageId)
         Picasso.get().setLoggingEnabled(true)
         Picasso.get()
@@ -46,5 +48,25 @@ class DetailActivity : AppCompatActivity() {
                     // Not implemented
                 }
             })
+
+        YoYo.with(Techniques.FlipInX)
+            .duration(1700)
+            .withListener(object: Animator.AnimatorListener {
+                override fun onAnimationRepeat(animation: Animator?) {
+                }
+
+                override fun onAnimationEnd(animation: Animator?) {
+                    cardText.text = text
+                }
+
+                override fun onAnimationCancel(animation: Animator?) {
+                }
+
+                override fun onAnimationStart(animation: Animator?) {
+                }
+
+            })
+            .playOn(cardImage)
+
     }
 }
